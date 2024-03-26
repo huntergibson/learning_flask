1c5a95806fb7367f9e824ff9ced99ac84f6854f2
Hunter Gibson <114520043+huntergibson@users.noreply.github.com>
2024-03-26 15:01:51 -0500

    wip - partner change

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello world, index"
+
+@app.route("/hunter")
+def hunter():
+    n1 = 10
+    n2 = 20
+    sum = n1 + n2
+    return str(sum)
+
+if __name__ == "__main__":
+    app.run(debug=True)
